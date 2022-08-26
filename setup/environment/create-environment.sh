1cf695d9224da23b73a15b6efda896dbfa349829
Hofmann Oliver <oliver.hofmann@ti8m.ch>
2022-08-27 01:28:06 +0200

    Add setup scripts for the python environment

---
+++ setup/environment/create-environment.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+conda create -n $1 -y python=$2
---
+++ setup/environment/setup-environment.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+envname="ovencam-volume-estimation"
+pythonversion=3.8
+
+echo "Start creating python environment"
+bash create-environment.sh $envname $pythonversion
+echo "Environment created"
+
+echo "Activate environment"
+source /anaconda/etc/profile.d/conda.sh
+conda activate $envname
+pip install --upgrade pip
+pip install ipykernel
+echo "Environment activated"
+
+echo "Start creating Notebook Kernel"
+ipython kernel install --user --name $envname --display-name "Python 3.8 (ovencam-volume-estimation)"
+echo "Kernel created"
+
+echo "Install dependencies"
+pip install -r requirements.txt
+echo "Dependencies installed"
+
