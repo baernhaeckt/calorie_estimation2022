515ad66702236023b7662d4785b8bd3100225431
Oliver Hofmann <oli_hofmann@outlook.com>
2022-08-27 00:04:09 +0000

    Change environment name

--- setup/environment/setup-environment.sh
+++ setup/environment/setup-environment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-envname="ovencam-volume-estimation"
+envname="calorie-estimation"
 pythonversion=3.8
 
 echo "Start creating python environment"
@@ -15,7 +15,7 @@ pip install ipykernel
 echo "Environment activated"
 
 echo "Start creating Notebook Kernel"
-ipython kernel install --user --name $envname --display-name "Python 3.8 (ovencam-volume-estimation)"
+ipython kernel install --user --name $envname --display-name "Python 3.8 (calorie-estimation)"
 echo "Kernel created"
 
 echo "Install dependencies"
