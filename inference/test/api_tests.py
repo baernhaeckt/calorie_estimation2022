8cf9433f183565c4b59c5936330f242defba733c
Hofmann Oliver <oliver.hofmann@ti8m.ch>
2022-08-28 10:44:46 +0200

    feat: Change API Test to the new URL

--- inference/test/api_tests.py
+++ inference/test/api_tests.py
@@ -1,7 +1,7 @@
 import requests
 
 if __name__ == '__main__':
-    url = "http://127.0.0.1:5000/api/v1/estimation"
+    url = "http://127.0.0.1:8000/api/v1/estimation"
     file = {
         "image": open("20151127_121613.jpg", "rb")
     }
